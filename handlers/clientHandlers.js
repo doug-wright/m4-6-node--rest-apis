67f545fa1c5563a64ed90ab6a1a9812fef64597b
Doug Wright <dw@nof49.ca>
2020-12-08 12:26:04 -0500

    Completed exercise 2

--- handlers/clientHandlers.js
+++ handlers/clientHandlers.js
@@ -1,4 +1,56 @@
 const { v4: uuidv4 } = require('uuid');
 const { clients } = require('../data/clients');
 
-// write your handlers here...
+// Get all clients
+const getAllClients = () => {
+  return { status: 'ok', data: clients };
+  // return { status: 'error', data: 'An error occured' };
+};
+
+// Get client by ID
+const getClientById = (id) => {
+  const index = clients.findIndex(client => client.id === id);
+  
+  if (index !== -1) {
+    return { status: 'ok', data: clients[index] };
+  } else {
+    return { status: 'error', data: 'Client not found'};
+  }
+}
+
+// Add new client
+const addClient = (query) => {
+  // Check for duplicate email address
+  if (clients.findIndex(client => client.email === query.email) === -1) {
+    const newClient = {
+      id: uuidv4(),
+      isActive: Boolean(query.isactive),
+      age: Number(query.age),
+      name: query.name,
+      gender: query.gender,
+      company: query.company,
+      email: query.email,
+      phone: '+1 (' + query.phone.slice(0, 3) + ') ' + query.phone.slice(3, 6) + '-' + query.phone.slice(6, 10),
+      address: query.address
+    }
+    clients.push(newClient);
+    return { status: 'ok', data: newClient.id };
+  } else {
+    return { status: 'error', data: 'Client not added: Duplicate email address' };
+  }
+}
+
+// Delete a client
+const deleteClient = (id) => {
+  // Check if client exists
+  const index = clients.findIndex(client => client.id === id);
+
+  if (index !== -1) {
+    clients.splice(index, 1);
+    return { status: 'ok', data: 'Client record deleted'};
+  } else {
+    return { status: 'error', data: 'Client not found'};
+  }
+}
+
+module.exports = { getAllClients, getClientById, addClient, deleteClient };
--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const { getAllClients, getClientById, addClient, deleteClient } = require('./handlers/clientHandlers');
+const { response } = require('express');
 
 express()
   .use(function (req, res, next) {
@@ -18,6 +20,57 @@ express()
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
-  // endpoints
+  // Exercise 2 endpoints
+
+  // Get all clients
+  .get('/clients', (req, res) => {
+    const response = getAllClients();
+
+    if (response.status === 'ok') {
+      res.status(200).json({ status: 200, clients: response.data });
+    } else {
+      res.status(200).json({ status: 200, message: response.data });
+    }
+  })
+
+  // Get specific client
+  .get('/clients/:id', (req, res) => {
+    const response = getClientById(req.params.id);
+
+    if (response.status === 'ok') {
+      res.status(200).json({ status: 200, clients: response.data });
+    } else {
+      res.status(200).json({ status: 200, message: response.data });
+    }
+  })
+
+  // Add a new client
+  .post('/clients', (req, res) => {
+    // Make sure all parameters are provided
+    const requiredParams = ['isactive', 'age', 'name', 'gender', 'company', 'email', 'phone', 'address'];
+
+    if (requiredParams.every(param => Object.keys(req.query).includes(param))) {
+      const response = addClient(req.query);
+
+      if (response.status === 'ok') {
+        res.status(201).json({ status: 201, message: response.data });
+      } else {
+        res.status(200).json({ status: 200, message: response.data });
+      }
+    } else {
+      res.status(400).json({ status: 400, message: 'Missing data'});
+    }
+  })
+
+  // Delete a client
+  .delete('/clients/:id', (req, res) => {
+    const response = deleteClient(req.params.id);
+
+    if (response.status === 'ok') {
+      res.status(200).json({ status: 200, message: response.data });
+    } else {
+      res.status(200).json({ status: 200, message: response.data });
+    }
+  })
 
   .listen(8000, () => console.log(`Listening on port 8000`));
